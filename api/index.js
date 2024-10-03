e6225734199e01b6768d0209adf5e826f70af944
prathamesh7376 <prathameshlokhande378@gmail.com>
2024-10-03 17:51:21 +0530

    adding all the changes

--- api/index.js
+++ api/index.js
@@ -1,5 +1,16 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log("Connected to MONDG_DB");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 const app = express();
 
 app.listen(5000, (req, res) => {
