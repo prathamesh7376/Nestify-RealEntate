f4f2e09ea8c93b7bc012394d46df089b34c18494
prathamesh7376 <prathameshlokhande378@gmail.com>
2024-10-03 22:48:22 +0530

    created signup ai route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/userRoute.js";
+import authRouter from "./routes/authRoute.js";
 dotenv.config();
 const PORT = process.env.PORT || 3000;
 mongoose
@@ -13,8 +14,10 @@ mongoose
     console.log(error);
   });
 const app = express();
+app.use(express.json());
 
 app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
 
 app.listen(PORT, (req, res) => {
   console.log(`Server is runningg on port ${PORT}`);
