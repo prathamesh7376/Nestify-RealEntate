a337163c174e833fac72136e7cfe33a14aa5fc01
prathamesh7376 <prathameshlokhande378@gmail.com>
2024-10-16 21:21:01 +0530

    added Unchange onsubmit for search Page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,6 +10,7 @@ import PrivateRoute from "./components/PrivateRoute";
 import CreateListing from "./pages/CreateListing";
 import UpdateListing from "./pages/UpdateListing";
 import Listing from "./pages/Listing";
+import Search from "./pages/Search";
 
 function App() {
   return (
@@ -21,6 +22,7 @@ function App() {
         <Route path="/signup" element={<Signup />} />
         <Route path="/about" element={<About />} />
         <Route path="/listing/:listingId" element={<Listing />} />
+        <Route path="/search" element={<Search />} />
 
         <Route path="/about" element={<PrivateRoute />}>
           <Route path="/profile" element={<Profile />} />
